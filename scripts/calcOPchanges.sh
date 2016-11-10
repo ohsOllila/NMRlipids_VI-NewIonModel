ee65270057be63ebb35cdf1f01b790eeb53a201e
Samuli <samuli.ollila@aalto.fi>
2016-11-10 17:49:34 +0200

    Add preliminary data

---
+++ scripts/calcOPchanges.sh
@@ -0,0 +1,2 @@
+awk -f calcOPchanges.awk ../Data/POPC/CaCl/SLIPID/OPvsCACLconsALLSCALED.dat  > ../Data/POPC/CaCl/SLIPID/SLIPIDcaclCONSchangeALLSCALED.dat 
+awk -f calcOPchanges.awk ../Data/POPC/CaCl/SLIPID/OPvsCACLconsHGSCALED.dat  > ../Data/POPC/CaCl/SLIPID/SLIPIDcaclCONSchangeHGSCALED.dat 
