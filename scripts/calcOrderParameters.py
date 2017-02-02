49a5039c425293335b19d4ffc2182ebfb7e6b566
Josef Melcr <melcr@uochb.cas.cz>
2017-02-02 14:02:09 +0100

    pythonic script for calculation OPs works on my test systems with ~4x speed increase compared to AWK script. Simple OP definition file is added to this commit, works for beta/alpha OPs in Slipids.

--- scripts/calcOrderParameters.py
+++ scripts/calcOrderParameters.py
@@ -16,18 +16,21 @@
 
 import MDAnalysis as mda
 import numpy as np
-#import scipy, math
-import matplotlib.pyplot as plt
-import cPickle
+import math
+#import matplotlib.pyplot as plt
+#import cPickle
 from optparse import OptionParser
+#import scipy, math
 
-try:
-    import read_xvg_calc_mean as rxvg
-except:
-    print "Couldn't read XVG-reading library, can't process Gromacs pullx.xvg files!"
+#try:
+    #import read_xvg_calc_mean as rxvg
+#except:
+    #print "Couldn't read XVG-reading library, can't process Gromacs pullx.xvg files!"
 
 k_b = 0.0083144621  #kJ/Mol*K
-
+f_conc=55430  # factor for calculating concentrations of salts from numbers of ions/waters; in mM/L
+bond_len_max=1.5  # in A, max distance between atoms for reasonable OP calculation
+bond_len_max_sq=bond_len_max**2
 
 #%%
 class OrderParameter:
@@ -57,40 +60,19 @@ class OrderParameter:
         self.traj = []  # for storing OPs
 
 
-
-    def get_OPs(self, mol):
-        """
-        Goes through every residue in a frame
-        (which is not necessary, but more robust and circumvents errors)
-        and
-        finds atom-name match with atoms A and B.
-        Calculates OP for the pair and
-        adds the OP to the OP's self.traj (which is a list)
-
-        Works with MDAnalysis library
-
-        Assuming 1 particular OP per 1 residue.
-        """
-        for res in mol.residues:
-            sel = res.select_atoms("name {atA} {atB}".format(atA=self.atAname, atB=self.atBname))
-            # check if we have only 2 atoms (A & B) selected
-            if len(sel.atoms) != 2:
-                raise UserWarning, "Selection >> name {atA} {atB} << \
-                contains {nat} atoms, but should contain exactly 2!".format(
-                atA=self.atAname, atB=self.atBname, nat=len(sel.atoms))
-            op = self.calc_OP(sel)
-            self.traj.append(op)
-            print "--", mol.atoms[0].position
-
-
     def calc_OP(self, atoms):
         """
         calculates Order Parameter according to equation
         S = 1/2 * (3*cos(theta)^2 -1)
         """
         vec = atoms[1].position - atoms[0].position
-        cos2 = vec[2]**2/sum(vec**2)
-        S = 0.5*(3*cos2-1)
+        d2 = np.square(vec).sum()
+        if d2>bond_len_max_sq:
+            raise UserWarning, "Atomic distance for atoms \
+ {at1} and {at2} in residue no. {resnr} is suspiciously \
+ long: {d}!\nPBC removed???".format(at1=atoms[0].name, at2=atoms[1].name, resnr=atoms[0].resid, d=math.sqrt(d2))
+        cos2 = vec[2]**2/d2
+        S = 0.5*(3.0*cos2-1.0)
         return S
 
 
@@ -100,8 +82,7 @@ class OrderParameter:
         Provides average and stddev of all OPs in self.traj
         """
         # convert to numpy array
-        data = np.array(self.traj)
-        return (data.mean(), data.std())
+        return (np.mean(self.traj), np.std(self.traj))
 
 
 def read_trajs(ordPars, top, trajs):
@@ -112,24 +93,37 @@ def read_trajs(ordPars, top, trajs):
     goes through every frame and
     evaluates each Order Parameter "S" from the list of OPs ordPars.
 
+    ordPars : list of OrderParameter class
+       each item in this list describes an Order parameter to be calculated in the trajectory
     top : str
         filename of a top file (e.g. conf.gro)
     trajs : list of strings
         filenames of trajectories
     """
+    # read-in topology and trajectory
     mol = mda.Universe(top, trajs)
+
+    # make atom selections for each OP and store it as its attribute for later use in trajectory
+    for op in ordPars.values():
+        # selection = pairs of atoms, split-by residues
+        selection = mol.select_atoms("resname {rnm} and name {atA} or name {atB}".format(
+                                    rnm=op.resname, atA=op.atAname, atB=op.atBname)
+                                    ).atoms.split("residue")
+        for res in selection:
+            # check if we have only 2 atoms (A & B) selected
+            if res.n_atoms != 2:
+                raise UserWarning, "Selection >> name {atA} {atB} << \
+                contains {nat} atoms, but should contain exactly 2!".format(
+                atA=op.atAname, atB=op.atBname, nat=len(sel.atoms))
+        op.selection = selection
+
+    # go through trajectory frame-by-frame
     for frame in mol.trajectory:
         for op in ordPars.values():
-            for res in mol.select_atoms("resname {rnm}".format(rnm=op.resname)).residues:
-                sel = res.select_atoms("name {atA} {atB}".format(atA=op.atAname, atB=op.atBname))
-                # check if we have only 2 atoms (A & B) selected
-                if len(sel.atoms) != 2:
-                    raise UserWarning, "Selection >> name {atA} {atB} << \
-                    contains {nat} atoms, but should contain exactly 2!".format(
-                    atA=op.atAname, atB=op.atBname, nat=len(sel.atoms))
-                S = op.calc_OP(sel)
+            for residue in op.selection:
+                S = op.calc_OP(residue)
                 op.traj.append(S)
-        print "--", mol.atoms[0].position
+        #print "--", mol.atoms[0].position
 
 
 
@@ -140,88 +134,70 @@ if __name__ == "__main__":
     # help message is automatically provided
     # type=string, action=store is default
     parser = OptionParser()
-    parser.add_option('-t', '--top',  dest='top_fname',  help='topology (gro, pdb) file name', default="confout.gro")
-    parser.add_option('-x', '--traj', dest='traj_fname', help='trajectory (xtc) file name', default="traj_comp.xtc")
+    parser.add_option('-i', '--inp',  dest='inp_fname',  help='input (OP definitions) file name', default="Headgroup_Glycerol_OPs.def")
+    parser.add_option('-t', '--top',  dest='top_fname',  help='topology (gro, pdb) file name', default="last_frame_nonwat.gro")
+    parser.add_option('-x', '--traj', dest='traj_fname', help='trajectory (xtc) file name', default="traj_nonwat_pbc.xtc")
+    parser.add_option('-o', '--out',  dest='out_fname',  help='output (OPs mean&std) file name', default="Headgroup_Glycerol_OPs.dat")
     opts, args = parser.parse_args()
 
+    # dictionary for storing of OrderParameter class instances (name-wise, of course)
     ordPars = {}
-    beta1OP  = OrderParameter("beta1",  "POPC", "C12", "H12A")
-    beta2OP  = OrderParameter("beta2",  "POPC", "C12", "H12B")
-    alpha1OP = OrderParameter("alpha1", "POPC", "C11", "H11A")
-    alpha2OP = OrderParameter("alpha2", "POPC", "C11", "H11B")
-    op_list = (beta1OP, beta2OP, alpha1OP, alpha2OP)
-    for op in op_list:
-        ordPars[op.name] = op
+    # read-in OP definitions from the input file
+    try:
+        with open(opts.inp_fname,"r") as f:
+            for line in f.readlines():
+                if not line.startswith("#"):
+                    items = line.split()
+                    op_name = items[0]
+                    resname = items[1]
+                    atAname = items[2]
+                    atBname = items[3]
+                    ordPars[op_name] = OrderParameter(op_name, resname, atAname, atBname)
+    except:
+        raise RuntimeError, "Couldn't read input file >> {inpf} <<".format(inpf=opts.inp_fname)
+
+
+#    ordPars = {}
+#    beta1OP  = OrderParameter("beta1",  "POPC", "C12", "H12A")
+#    beta2OP  = OrderParameter("beta2",  "POPC", "C12", "H12B")
+#    alpha1OP = OrderParameter("alpha1", "POPC", "C11", "H11A")
+#    alpha2OP = OrderParameter("alpha2", "POPC", "C11", "H11B")
+#    op_list = (beta1OP, beta2OP, alpha1OP, alpha2OP)
+#    for op in op_list:
+#        ordPars[op.name] = op
 
 #%%
-
     read_trajs(ordPars, opts.top_fname, opts.traj_fname)
 
 #%%
 
+    print "OP Name     mean     stddev "
+    print "----------------------------"
     for op in ordPars.values():
-        print op.name, op.get_avg_std_OP
-
-#%%
-
-
-#############################################
-## ENDED HERE CONTINUE NEXT TIME...
-## note: code above worked on test files
-#############################################
+        (op.avg, op.std) = op.get_avg_std_OP
+        print op.name, op.avg, op.std
 
 
-    print "Saving the profile into a pickle-object and text file..."
-
-    # "aliases" for plotting and saving
-    #dTRAM data
-    fep_dtram = dtram_obj.f_i
-    fep_dtram_shift = np.copy(fep_dtram)
-    fep_dtram_shift -= fep_dtram_shift.min()
-
-    with open(opts.out_file_name+"_dTRAM-FEP_windows.pickle","w") as f : cPickle.dump(dtram_obj.f_K,f)
-    with open(opts.out_file_name+"_dTRAM-FEP.pickle","w") as f : cPickle.dump(fep_dtram_shift,f)
-    with open(opts.out_file_name+"_dTRAM-FEP.dat","w") as f:
-        for i,c in enumerate(gridpoints):
-            line = str(c)+"   "+str(fep_dtram_shift[i])+"\n"
-            f.write( line )
-
-
-
-    # In[46]:
-
-    print "Plotting dTRAM, and WHAM data if present ... "
-
-    # Load the result from WHAM: (generated with the same set and with the same converg. criterion 1.0E-6, 90 bins ..)
     try:
-        wham_data = np.loadtxt(opts.wham_fep_fname)
-        cnz_wham = wham_data[:,0]
-        kT = k_b*sim_data.sims[0]['temperature']
-        fep_wham = wham_data[:,1]/kT # scale by kT
-        plt.plot( cnz_wham, fep_wham, '-', color='green', lw=2.0, label="WHAM" )
-	try:
-		fep_wham_err1 =  wham_data[:,2]/kT + fep_wham
-		fep_wham_err2 = -wham_data[:,2]/kT + fep_wham
-		plt.fill_between( cnz_wham, fep_wham_err1, fep_wham_err2, color='green', alpha=0.2)
-	except:
-		print "Couldn't read-in the errorbars for WHAM-data."
+        with open(opts.out_fname,"w") as f:
+            f.write("OP_name    resname    atom1    atom2    OP_mean   OP_stddev\n\
+------------------------------------------------------------\n")
+            for op in ordPars.values():
+                f.write( "   ".join([op.name, op.resname, op.atAname, op.atBname, str(op.avg), str(op.std), "\n"]) )
+        print "OrderParameters written to >> {fname} <<".format(fname=opts.out_fname)
     except:
-        print "Wham data missing or just something wrong happened in the process. Look into the code, lad. "
+        print "Problems writing main output file."
 
-    plt.plot( gridpoints, fep_dtram_shift, '-', color='black', lw=2.0, label="dTRAM" )
 
-    plt.legend( loc='upper left', fontsize=10 )
-    plt.xlabel( r"$pitch$ [deg]", fontsize=12 )
-    plt.ylabel( r"$G$ [kT]", fontsize=12 )
-
-    plt.savefig(opts.out_file_name+"_dTRAM_WHAM.png", papertype="letter", dpi=300)
-
-    # plotting dTRAM - Jacobi corrected (assuming Pitch-polar coordinates)
-    kT = k_b*sim_data.sims[0]['temperature']
-    plt.plot( gridpoints, fep_dtram_shift + kT*np.log(np.sin(gridpoints/180.0*math.pi)), '-', color='blue', lw=2.0, label="dTRAM Jacobi corrected" )
-
-    plt.legend( loc='upper left', fontsize=10 )
-    plt.xlabel( r"$pitch$ [deg]", fontsize=12 )
-    plt.ylabel( r"$G$ [kT]", fontsize=12 )
-
-    plt.savefig(opts.out_file_name+"_dTRAM_WHAM_JacobiCorr.png", papertype="letter", dpi=300)
+    #conc = f_conc*nion/nwat
+    try:
+        conc_formatted_line = "conc  {b1} 0  {b2} 0    {a1} 0  {a2} 0".format(
+                              b1=ordPars['beta1'].avg, b2=ordPars['beta2'].avg,
+                              a1=ordPars['alpha1'].avg, a2=ordPars['alpha2'].avg)
+        print
+        print "Single line format:\nconc  beta1 0  beta2 0  alpha1 0  alpha2 0"
+        print conc_formatted_line
+        with open(opts.out_fname+".line","w") as f:
+            f.write(conc_formatted_line)
+    except:
+        print "Problems writing the beta-alpha single line format file."
