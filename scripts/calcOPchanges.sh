2e47c6544c2e7d24c57a8779c80f1a7b3eb66d9b
Josef Melcr <melcr@uochb.cas.cz>
2017-02-27 12:50:38 +0100

    New data on Lipid14 headgr+glycerol (and carbonyls) scaled 0.85. Added OP raw results into Data folder for many previous simulations. Updated OP-figure.

--- scripts/calcOPchanges.sh
+++ scripts/calcOPchanges.sh
@@ -9,6 +9,7 @@ awk -f calcOPchanges.awk ../Data/POPC/CaCl/lipid14/OPvsCACLconsHGSCALED.dat  > .
 awk -f calcOPchanges.awk ../Data/POPC/CaCl/lipid14/OPvsCACLconsHGSCALED_0.85.dat  > ../Data/POPC/CaCl/lipid14/LIPID14caclCONSchangeHGSCALED_0.85.dat 
 awk -f calcOPchanges.awk ../Data/POPC/CaCl/lipid14/OPvsCACLconsHGSCALED_0.80.dat  > ../Data/POPC/CaCl/lipid14/LIPID14caclCONSchangeHGSCALED_0.80.dat 
 awk -f calcOPchanges.awk ../Data/POPC/CaCl/lipid14/OPvsCACLconsOxySCALED.dat  > ../Data/POPC/CaCl/lipid14/LIPID14caclCONSchangeOxySCALED.dat 
+awk -f calcOPchanges.awk ../Data/POPC/CaCl/lipid14/OPvsCACLconsHGglycSCALED_0.85.dat  > ../Data/POPC/CaCl/lipid14/LIPID14caclCONSchangeHGglycSCALED_0.85.dat 
 
 awk -f calcOPchanges.awk ../Data/POPC/NaCl/Slipids-VIS/OPvsNACLconsALLSCALED.dat  > ../Data/POPC/NaCl/Slipids-VIS/SLIPIDnaclCONSchangeALLSCALED.dat 
 awk -f calcOPchanges.awk ../Data/POPC/NaCl/Slipids-VIS/OPvsNACLconsHGSCALED.dat  > ../Data/POPC/NaCl/Slipids-VIS/SLIPIDnaclCONSchangeHGSCALED.dat 
@@ -21,3 +22,4 @@ awk -f calcOPchanges.awk ../Data/POPC/NaCl/lipid14/OPvsNACLconsORIG.dat  > ../Da
 awk -f calcOPchanges.awk ../Data/POPC/NaCl/lipid14/OPvsNACLconsHGSCALED_0.85.dat  > ../Data/POPC/NaCl/lipid14/LIPID14naclCONSchangeHGSCALED_0.85.dat 
 awk -f calcOPchanges.awk ../Data/POPC/NaCl/lipid14/OPvsNACLconsHGSCALED_0.80.dat  > ../Data/POPC/NaCl/lipid14/LIPID14naclCONSchangeHGSCALED_0.80.dat 
 awk -f calcOPchanges.awk ../Data/POPC/NaCl/lipid14/OPvsNACLconsOxySCALED.dat  > ../Data/POPC/NaCl/lipid14/LIPID14naclCONSchangeOxySCALED.dat 
+awk -f calcOPchanges.awk ../Data/POPC/NaCl/lipid14/OPvsNACLconsHGglycSCALED_0.85.dat  > ../Data/POPC/NaCl/lipid14/LIPID14naclCONSchangeHGglycSCALED_0.85.dat 
