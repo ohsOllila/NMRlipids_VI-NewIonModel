0b4bf15623b7d489ff13655ba34d8f45e4550863
Josef Melcr <melcr@uochb.cas.cz>
2017-08-17 07:29:13 +0200

    Small update to python script for getting bulk concentrations - now provides values for cation, anion, water.

--- scripts/get_conc_ion_bulk.py
+++ scripts/get_conc_ion_bulk.py
@@ -21,3 +21,16 @@ print "bulk conc. = ({: 4.0f} +/- {: 3.0f}) mmol/L".format(mean, stdev)
 with open("conc_ion_bulk_mmolL.dat", "w") as f:
     f.write(" ".join([str(mean), str(stdev)]))
 
+wat_dens = densities[:,3]
+mean_wat  = wat_dens[:10*i].mean() /denom_conc *1000.0
+stdev_wat = wat_dens[:10*i].std()  /denom_conc *1000.0
+
+with open("conc_wat_bulk_mmolL.dat", "w") as f:
+    f.write(" ".join([str(mean_wat), str(stdev_wat)]))
+
+anion_dens = densities[:,2]
+mean_anion  = anion_dens[:10*i].mean() /denom_conc *1000.0
+stdev_anion = anion_dens[:10*i].std()  /denom_conc *1000.0
+
+with open("conc_anion_bulk_mmolL.dat", "w") as f:
+    f.write(" ".join([str(mean_anion), str(stdev_anion)]))
